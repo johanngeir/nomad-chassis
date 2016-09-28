75ae47e701a6df3893a05373cc33aa3b4249c98f
Jóhann Geir Jónsson <johanngj@siminn.is>
2016-09-28 23:59:52 +0000

    Added nomad config example

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ usermod -aG docker $(whoami)
 # install consul
 curl https://releases.hashicorp.com/consul/0.7.0/consul_0.7.0_linux_amd64.zip | sudo bsdtar -xzf- -C /usr/local/bin/
 mkdir /etc/consul.d
+mkdir /etc/nomad.d
 
 # install nomad 
 curl https://releases.hashicorp.com/nomad/0.4.1/nomad_0.4.1_linux_amd64.zip | sudo bsdtar -xzf- -C /usr/local/bin/
@@ -20,5 +21,20 @@ curl https://releases.hashicorp.com/nomad/0.4.1/nomad_0.4.1_linux_amd64.zip | su
 chmod guo+x /usr/local/bin/*
 
 ipaddr=$(ip addr | awk '/inet/ && /ens/{sub(/\/.*$/,"",$2); print $2}')
-echo Consul start:
+echo \n\nConsul seed start:
 echo consul agent -server -bootstrap-expect 1 -data-dir /tmp/consul -node=agent-one -bind=$ipaddr -config-dir /etc/consul.d
+
+echo \nNomad config:
+echo "echo '# Increase log verbosity
+log_level = "DEBUG"
+
+# Setup data dir
+data_dir = "/tmp/server1"
+
+# Enable the server
+server {
+    enabled = true
+
+    # Self-elect, should be 3 or 5 for production
+    bootstrap_expect = 1
+}' > /etc/nomad.d/server.hcl "
