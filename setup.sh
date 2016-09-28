9121e62d86f3a527f49668b5103f610eae84343b
Jóhann Geir Jónsson <johanngj@siminn.is>
2016-09-28 23:31:44 +0000

    Added nomad and consul

--- setup.sh
+++ setup.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
+sudo su -
 
-sudo locale-gen is_IS.UTF-8
+locale-gen is_IS.UTF-8
 
 apt update
 apt upgrade
-apt install curl
+apt install curl unzip bsdtar
 
+# install docker
 curl -sSL https://get.docker.com/ | sh
 usermod -aG docker $(whoami)
+
+# install consul
+curl https://releases.hashicorp.com/consul/0.7.0/consul_0.7.0_linux_amd64.zip | sudo bsdtar -xzf- -C /usr/local/bin/
+
+# install nomad 
+curl https://releases.hashicorp.com/nomad/0.4.1/nomad_0.4.1_linux_amd64.zip | sudo bsdtar -xzf- -C /usr/local/bin/
+
+chmod guo+x /usr/local/bin/*
