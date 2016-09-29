6bba547cf7eccce3ecd0a06e5787c2d86b95dcb5
Jóhann Geir Jónsson <johanngj@siminn.is>
2016-09-29 00:36:41 +0000

    chmod

--- setup.sh
+++ setup.sh
@@ -18,8 +18,8 @@ mkdir /etc/nomad.d
 # install nomad 
 curl https://releases.hashicorp.com/nomad/0.4.1/nomad_0.4.1_linux_amd64.zip | bsdtar -xzf- -C /usr/local/bin/
 
-chmod guo+x /usr/local/bin/*
-
 wget -O /etc/nomad.d/server.hcl https://raw.githubusercontent.com/johanngeir/nomad-chassis/master/server.hcl
 wget -O /usr/local/bin/seed-consul.sh https://raw.githubusercontent.com/johanngeir/nomad-chassis/master/seed-consul.sh
 wget -O /usr/local/bin/start-nomad.sh https://raw.githubusercontent.com/johanngeir/nomad-chassis/master/start-nomad.sh
+
+chmod guo+x /usr/local/bin/*
