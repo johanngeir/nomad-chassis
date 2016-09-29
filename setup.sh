add60b9aae4b8346addd990d0814a704979f2924
Jóhann Geir Jónsson <johanngj@siminn.is>
2016-09-29 00:35:03 +0000

    Removed sudo since sudo is needed for the whole script

--- setup.sh
+++ setup.sh
@@ -11,15 +11,15 @@ curl -sSL https://get.docker.com/ | sh
 usermod -aG docker $(whoami)
 
 # install consul
-curl https://releases.hashicorp.com/consul/0.7.0/consul_0.7.0_linux_amd64.zip | sudo bsdtar -xzf- -C /usr/local/bin/
+curl https://releases.hashicorp.com/consul/0.7.0/consul_0.7.0_linux_amd64.zip | bsdtar -xzf- -C /usr/local/bin/
 mkdir /etc/consul.d
 mkdir /etc/nomad.d
 
 # install nomad 
-curl https://releases.hashicorp.com/nomad/0.4.1/nomad_0.4.1_linux_amd64.zip | sudo bsdtar -xzf- -C /usr/local/bin/
+curl https://releases.hashicorp.com/nomad/0.4.1/nomad_0.4.1_linux_amd64.zip | bsdtar -xzf- -C /usr/local/bin/
 
 chmod guo+x /usr/local/bin/*
 
-sudo wget -O /etc/nomad.d/server.hcl https://raw.githubusercontent.com/johanngeir/nomad-chassis/master/server.hcl
-sudo wget -O /usr/local/bin/seed-consul.sh https://raw.githubusercontent.com/johanngeir/nomad-chassis/master/seed-consul.sh
-sudo wget -O /usr/local/bin/start-nomad.sh https://raw.githubusercontent.com/johanngeir/nomad-chassis/master/start-nomad.sh
+wget -O /etc/nomad.d/server.hcl https://raw.githubusercontent.com/johanngeir/nomad-chassis/master/server.hcl
+wget -O /usr/local/bin/seed-consul.sh https://raw.githubusercontent.com/johanngeir/nomad-chassis/master/seed-consul.sh
+wget -O /usr/local/bin/start-nomad.sh https://raw.githubusercontent.com/johanngeir/nomad-chassis/master/start-nomad.sh
